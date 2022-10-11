e6531ea644fe91375f37eb68b0073694b2d8b5a2
Haotian Chen <chtunsw@gmail.com>
2022-10-12 02:02:52 +1100

    resize

--- dino.py
+++ dino.py
@@ -8,6 +8,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from PIL import Image
 
 game_url = "chrome://dino"
+resolution = (256, 256)
 
 class Game():
     def __init__(self):
@@ -27,8 +28,9 @@ if __name__ == "__main__":
     game = Game()
     game.open()
     frame = game.get_frame()
-    img = Image.open(io.BytesIO(frame))
+    img = Image.open(io.BytesIO(frame)).convert("L").resize(resolution)
     img_array = np.array(img)
+    print(img_array.shape)
     cv2.imshow("screen", img_array)
     cv2.waitKey()
     
